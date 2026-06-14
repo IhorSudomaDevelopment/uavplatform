fedd30d521e536c5ea5cbe691a343ad99f569824
IhorSudomaDevelopment <i.sudoma.develop@gmail.com>
2026-06-15 01:42:59 +0300

    [DEV-28] Locale changes

---
+++ lang/vendor/filament-forms/uk/components.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'select' => [
+        'placeholder' => 'Оберіть значення',
+        'searching_message' => 'Пошук...',
+        'no_search_results_message' => 'Нічого не знайдено',
+    ],
+];
