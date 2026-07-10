97d7d2ea0fab8af64c918a67f168035d090d8e58
IhorSudomaDevelopment <i.sudoma.develop@gmail.com>
2026-07-10 21:03:20 +0300

    [FIX] Test locale global changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -23,5 +24,6 @@ class AppServiceProvider extends ServiceProvider
         if (app()->environment('production')) {
             URL::forceScheme('https');
         }
+        App::setLocale('uk');
     }
 }
